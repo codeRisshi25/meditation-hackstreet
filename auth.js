a9acacb456adfa3c956cc4a988eaa27d8733e61b
Risshi25 <risshirajsen@gmail.com>
2024-02-03 17:04:57 +0530

    changes 1.01

---
+++ auth.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+const path = require("path");
+
+dotenv.config({ path: "./.env" });
+
+const app = express();
+
+const db = mysql.createConnection({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE,
+});
+
+const publicDirectory = path.join(__dirname, "./public");
+app.use(express.static(publicDirectory));
+
+app.set("view engine", "hbs");
+
+db.connect((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Connected to database");
+  }
+});
+
+app.get("/", (req, res) => {
+  res.render("/index");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
